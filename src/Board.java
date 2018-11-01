5bfa1d9c505bad8848f711c1bf0d08ae4c06a230
FranciscoThiesen <franciscogthiesen@gmail.com>
2018-10-31 22:25:21 -0300

    Separating squares by its colors. Next step is to draw all white squares and the center of the board. We also need to figure out why the black lines appear only in a few of the squares.

--- src/Board.java
+++ src/Board.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import javax.swing.*;
 import javax.tools.Tool;
+import java.util.*;
 
 public class Board extends JFrame {
 
@@ -24,28 +25,70 @@ public class Board extends JFrame {
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
 
-        BoardSquare[] sanctuarySquares = new BoardSquare[4];
-        Coordinate2D[] sanctuaryTopLeftCoords = new Coordinate2D[4];
-        Color[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.yellow};
-        for(int i = 0; i < 4; i++ ) {
-            if(i == 0) sanctuaryTopLeftCoords[i] = new Coordinate2D(0, 0);
-            else if(i == 1) sanctuaryTopLeftCoords[i] = new Coordinate2D(0, 360);
-            else if(i == 2) sanctuaryTopLeftCoords[i] = new Coordinate2D(360, 0);
-            else sanctuaryTopLeftCoords[i] = new Coordinate2D(360, 360);
-            sanctuarySquares[i] = new BoardSquare(sanctuaryTopLeftCoords[i], 240, colors[i]);
+        ArrayList<BoardSquare> redSquares = new ArrayList<BoardSquare>();
+        ArrayList<BoardSquare> blueSquares = new ArrayList<BoardSquare>();
+        ArrayList<BoardSquare> greenSquares = new ArrayList<BoardSquare>();
+        ArrayList<BoardSquare> yellowSquares = new ArrayList<BoardSquare>();
+
+        redSquares.add( new BoardSquare(new Coordinate2D(0, 0), 240, Color.RED ) );
+        blueSquares.add( new BoardSquare(new Coordinate2D(0, 360), 240, Color.BLUE ) );
+        greenSquares.add( new BoardSquare(new Coordinate2D(360, 0), 240, Color.GREEN ) );
+        yellowSquares.add( new BoardSquare(new Coordinate2D(360, 360), 240, Color.YELLOW) );
+
+        redSquares.add( new BoardSquare(new Coordinate2D(40, 240), 40, Color.RED) );
+        blueSquares.add( new BoardSquare(new Coordinate2D(240, 520), 40, Color.BLUE) );
+        greenSquares.add( new BoardSquare(new Coordinate2D(320, 40), 40, Color.GREEN) );
+        yellowSquares.add( new BoardSquare(new Coordinate2D(520, 320), 40, Color.YELLOW) );
+
+        for(int i = 0; i < 5; i++) {
+            redSquares.add( new BoardSquare(new Coordinate2D((i + 1) * 40, 280), 40, Color.RED) );
+            blueSquares.add( new BoardSquare(new Coordinate2D(280, 560 - ( (i + 1) * 40) ), 40, Color.BLUE) );
+            greenSquares.add( new BoardSquare(new Coordinate2D(280, (i + 1) * 40), 40, Color.GREEN) );
+            yellowSquares.add( new BoardSquare(new Coordinate2D(560 - ( ( i + 1) * 40), 280), 40, Color.YELLOW) );
         }
 
 
         g2D.setStroke(new BasicStroke(1) );
-        for(int i = 0; i < 4; i++) {
-            g2D.setColor(Color.BLACK);
-            g2D.drawRect(sanctuarySquares[i].topLeftPosition.x, sanctuarySquares[i].topLeftPosition.y, sanctuarySquares[i].sideLength, sanctuarySquares[i].sideLength);
-            g2D.setColor(sanctuarySquares[i].squareColor);
-            g2D.fillRect(sanctuarySquares[i].topLeftPosition.x, sanctuarySquares[i].topLeftPosition.y, sanctuarySquares[i].sideLength, sanctuarySquares[i].sideLength);
-        }
-        //g2D.setColor(Color.BLACK);
 
-        //g2D.fillRect(500, 0, 100, 100);
+        for(BoardSquare b : redSquares) {
+            int x = b.topLeftPosition.x;
+            int y = b.topLeftPosition.y;
+            int len = b.sideLength;
+            g2D.setColor(Color.BLACK);
+            g2D.drawRect(x, y, len, len);
+            g2D.setColor(Color.RED);
+            g2D.fillRect(x, y, len, len);
+        }
+
+        for(BoardSquare b : blueSquares) {
+            int x = b.topLeftPosition.x;
+            int y = b.topLeftPosition.y;
+            int len = b.sideLength;
+            g2D.setColor(Color.BLACK);
+            g2D.drawRect(x, y, len, len);
+            g2D.setColor(Color.BLUE);
+            g2D.fillRect(x, y, len, len);
+        }
+
+        for(BoardSquare b : greenSquares) {
+            int x = b.topLeftPosition.x;
+            int y = b.topLeftPosition.y;
+            int len = b.sideLength;
+            g2D.setColor(Color.BLACK);
+            g2D.drawRect(x, y, len, len);
+            g2D.setColor(Color.GREEN);
+            g2D.fillRect(x, y, len, len);
+        }
+
+        for(BoardSquare b : yellowSquares) {
+            int x = b.topLeftPosition.x;
+            int y = b.topLeftPosition.y;
+            int len = b.sideLength;
+            g2D.setColor(Color.BLACK);
+            g2D.drawRect(x, y, len, len);
+            g2D.setColor(Color.YELLOW);
+            g2D.fillRect(x, y, len, len);
+        }
 
     }
 
--- src/BoardSquare.java
+++ src/BoardSquare.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class BoardSquare {
     public Coordinate2D topLeftPosition;
@@ -15,5 +16,10 @@ public class BoardSquare {
         double minY = topLeftPosition.y - sideLength;
         double maxX = topLeftPosition.x + sideLength;
         return (coord.x >= topLeftPosition.x && coord.x <= maxX && coord.y <= topLeftPosition.y && coord.y >= minY);
+    }
+
+    public Rectangle2D toRect() {
+        Rectangle2D r2D = new Rectangle2D.Float( (float) this.topLeftPosition.x, (float) this.topLeftPosition.y, (float) this.sideLength, (float) this.sideLength);
+        return r2D;
     }
 }
